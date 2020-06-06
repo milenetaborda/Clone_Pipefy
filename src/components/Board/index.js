981abd98e48e31a832af0a7f0bd0a6d572b70b1b
Milene Taborda <milenetaborda1@outlook.com>
2020-06-06 11:36:25 -0300

    Funcionalidade do useDrag implementada

---
+++ src/components/Board/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { loadLists } from '../../services/api';
+
+import { Container } from './styles';
+import List from '../List';
+
+const lists = loadLists();
+
+function Board() {
+  return ( 
+    <Container>
+      {lists.map(list => <List key={list.title} data={list} />)}
+    </Container>
+  )
+}
+
+export default Board;
